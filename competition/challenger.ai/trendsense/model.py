ad6e28dc1819d3edc5783bd85b84c41e4f488ce7
lipixun <lipixun@gmail.com>
2017-09-11 15:45:04 +0800

    update

--- competition/challenger.ai/trendsense/model.py
+++ competition/challenger.ai/trendsense/model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding=utf8
+# pylint: disable=wrong-import-position, not-context-manager
 
 """ The model
     Author: lipixun
@@ -17,3 +18,125 @@
 import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
+
+import logging
+
+from os import makedirs
+from os.path import isdir
+
+import h5py
+import tensorflow as tf
+import tftrainer
+import tfutils
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger("preprocessor")
+
+Path = "model"
+XSize = 88
+L2Beta = 1e-3
+BatchSize = 512
+IsTraining = tftrainer.getIsTrainingVar()
+
+class Model(object):
+    """The model object
+    """
+    def __init__(self):
+        """Create a new Model
+        """
+        # Define the graph inputs
+        self.X = tf.placeholder(shape=(None, XSize), dtype=tf.float32, name="x")
+        self.Y = tf.placeholder(shape=(None, ), dtype=tf.float32, name="y")
+        self.W = tf.placeholder(shape=(None, ), dtype=tf.float32, name="w")
+        # Define layers
+        with tf.variable_scope("layer1"):
+            inp = tf.cond(IsTraining, true_fn=lambda: tf.nn.dropout(self.X, 0.8), false_fn=lambda: self.X)
+            self.layer1W = tf.get_variable("W", shape=(XSize, 1000), initializer=tf.truncated_normal_initializer())
+            self.layer1B = tf.get_variable("b", shape=(1000,))
+            self.layer1 = tf.nn.xw_plus_b(inp, self.layer1W, self.layer1B)
+        with tf.variable_scope("layer2"):
+            inp = tf.cond(IsTraining, true_fn=lambda: tf.nn.dropout(self.layer1, 0.8), false_fn=lambda: self.layer1)
+            self.layer2W = tf.get_variable("W", shape=(1000, 200), initializer=tf.truncated_normal_initializer())
+            self.layer2B = tf.get_variable("b", shape=(200,))
+            self.layer2 = tf.nn.xw_plus_b(inp, self.layer2W, self.layer2B)
+        # Output
+        with tf.variable_scope("output"):
+            inp = tf.cond(IsTraining, true_fn=lambda: tf.nn.dropout(self.layer2, 0.8), false_fn=lambda: self.layer2)
+            self.outputW = tf.get_variable("W", shape=(200, 1), initializer=tf.truncated_normal_initializer())
+            self.outputB = tf.get_variable("b", shape=(1,))
+            self.outputLogits = tf.nn.xw_plus_b(inp, self.outputW, self.outputB)
+            self.output = tf.nn.sigmoid(self.outputLogits)
+        # Loss & training method
+        self.logitLoss = tf.reshape(self.W, shape=(-1, 1)) * tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.reshape(self.Y, shape=(-1,1)), logits=self.outputLogits)
+        self.loss = tf.reduce_mean(self.logitLoss, name="loss")
+        self.lossWithL2 = self.loss + L2Beta * (tf.nn.l2_loss(self.layer1W) + tf.nn.l2_loss(self.layer2W) + tf.nn.l2_loss(self.outputW))
+        self.trainOp = tftrainer.AutoTrainOp(
+            name="default",
+            losses={"default": self.loss},
+            metrics={"logloss": tftrainer.metric.TFStreamingMetric(tf.contrib.metrics.streaming_mean, "logloss", self.logitLoss)},
+            optimizers=[ tfutils.optimizers.clipGradientByGlobalNorm(tf.train.AdamOptimizer(), self.lossWithL2, 5.0) ],
+        )
+
+    def train(self, inp):
+        """Train this model
+        """
+        # Open input data
+        logger.info("Load input data: %s", inp)
+        h5f = h5py.File(inp, "r")
+        trainFeeder = tftrainer.HDF5Feeder(h5f, { self.X: "x", self.W: "w", self.Y: "y" }, BatchSize, shuffle=True, queue=tftrainer.SimpleFeedQueue(1000))
+        testFeeder  = tftrainer.HDF5Feeder(h5f, { self.X: "test-x", self.W: "test-w", self.Y: "test-y" }, BatchSize, shuffle=True, queue=tftrainer.SimpleFeedQueue(1000))
+        if not isdir(Path):
+            makedirs(Path)
+        # Start train
+        logger.info("Train the model")
+        trainer = tftrainer.Trainer(Path)
+        trainer.train(trainOps=self.trainOp, validateOps=self.trainOp, trainFeeder=trainFeeder, validateFeeder=testFeeder, testFeeder=testFeeder, maxEpoch=100)
+        #trainer.train(trainOps=self.trainOp, validateOps=self.trainOp, trainFeeder=trainFeeder, validateFeeder=trainFeeder, maxEpoch=50)
+
+    def predict(self, inp, out):
+        """Predict by this model
+        """
+        # Load model
+        logger.info("Load model")
+        trainer = tftrainer.Trainer(Path)
+        # Load input data
+        logger.info("Load input data: %s", inp)
+        h5f = h5py.File(inp, "r")
+        feeder = tftrainer.HDF5Feeder(h5f, {self.X: "x"}, BatchSize, shuffle=True, queue=tftrainer.SimpleFeedQueue(1000))
+        # Predict
+        logger.info("Predict")
+        outputs = trainer.run(outs=self.output, feeder=feeder)
+        # Write file
+        logger.info("Write file")
+        with open(out, "wb") as fd:
+            for output in outputs:
+                for value in output:
+                    print >>fd, value[0]
+
+if __name__ == "__main__":
+
+    from argparse import ArgumentParser
+
+    def getArguments():
+        """Get arguments
+        """
+        parser = ArgumentParser(description="Model")
+        subParsers = parser.add_subparsers(dest="action")
+        trainParser = subParsers.add_parser("train", help="Train model")
+        trainParser.add_argument("-i", "--input", dest="input", required=True, help="The input data")
+        predParser = subParsers.add_parser("pred", help="Predict by model")
+        predParser.add_argument("-i", "--input", dest="input", required=True, help="The input data")
+        predParser.add_argument("-o", "--output", dest="output", required=True, help="The output data")
+        # Done
+        return parser.parse_args()
+
+    def main():
+        """The main entry
+        """
+        args = getArguments()
+        if args.action == "train":
+            return Model().train(args.input)
+        else:
+            return Model().predict(args.input, args.output)
+
+    main()
--- competition/challenger.ai/trendsense/preproc.py
+++ competition/challenger.ai/trendsense/preproc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding=utf8
+# pylint: disable=wrong-import-position
 
 """ Preprocess the data
     Author: lipixun
@@ -19,6 +20,81 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
 
+import logging
+
+from cPickle import load, dump
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import RobustScaler, Imputer
+
+TestERA = 10.0
+BatchSize = 512
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger("preprocessor")
+
+def runTrain(args):
+    """Run preprocess for training
+    """
+    logger.info("Load input file: %s", args.input)
+    df = pd.read_csv(args.input, dtype=np.float32, engine="c") # pylint: disable=no-member
+    # Split data into train and test
+    data = df.values
+    train = data[data[:,-1] != TestERA]
+    trainX = train[:,1:-4]
+    trainW = train[:,-4]
+    trainY = train[:,-3]
+    test = data[data[:,-1] == TestERA]
+    testX = test[:,1:-4]
+    testW = test[:,-4]
+    testY = test[:,-3]
+    logger.info("Get Train data X%s W%s Y%s Test data X%s W%s Y%s", trainX.shape, trainW.shape, trainY.shape, testX.shape, testW.shape, testY.shape)
+    # Create the preprocessors
+    pl = Pipeline([ ("scaler", RobustScaler()), ("imputer", Imputer()) ])
+    _trainX = pl.fit_transform(trainX)
+    _testX = pl.transform(testX)     # NOTE: MUST not fit by the test data
+    # Show some examples
+    print "="*10, "TrainX Before Transform", "="*10
+    print trainX[0]
+    print "="*10, "TrainX After Transform", "="*10
+    print _trainX[0]
+    # Write hdf5 file
+    logger.info("Write hdf5 file: %s", args.output)
+    h5f = h5py.File(args.output, "w")
+    h5f.create_dataset("x", data=_trainX, chunks=(BatchSize, _trainX.shape[1]))
+    h5f.create_dataset("w", data=trainW, chunks=(BatchSize, ))
+    h5f.create_dataset("y", data=trainY, chunks=(BatchSize, ))
+    h5f.create_dataset("test-x", data=_testX, chunks=(BatchSize, _testX.shape[1]))
+    h5f.create_dataset("test-w", data=testW, chunks=(BatchSize, ))
+    h5f.create_dataset("test-y", data=testY, chunks=(BatchSize, ))
+    h5f.close()
+    # Write model file
+    logger.info("Write model file: %s", args.model)
+    with open(args.model, "wb") as fd:
+        dump(pl, fd)
+
+def runApply(args):
+    """Run preprocess for applying
+    """
+    # Load model
+    logger.info("Load model file: %s", args.model)
+    with open(args.model, "rb") as fd:
+        pl = load(fd)
+    # Load file
+    logger.info("Load input file: %s", args.input)
+    df = pd.read_csv(args.input, dtype=np.float32, engine="c") # pylint: disable=no-member
+    X = pl.tranform(df.values[:,:-1])
+    logger.info("Get data X%s", X.shape)
+    # Write file
+    logger.info("Write hdf5 file: %s", args.output)
+    h5f = h5py.File(args.output, "w")
+    h5f.create_dataset("x", data=X, chunks=(BatchSize, X.shape[1]))
+    h5f.close()
+
 if __name__ == "__main__":
 
     from argparse import ArgumentParser
@@ -30,8 +106,14 @@ if __name__ == "__main__":
         subParsers = parser.add_subparsers(dest="action")
         # Train
         trainParser = subParsers.add_parser("train", help="Run train preprocess")
+        trainParser.add_argument("-i", "--input", dest="input", required=True, help="The input csv file")
+        trainParser.add_argument("-m", "--model", dest="model", default="preproc-model.bin", help="The preprocess model")
+        trainParser.add_argument("-o", "--output", dest="output", required=True, help="The output hdf5 file")
         # Apply
         applyParser = subParsers.add_parser("apply", help="Apply preprocess logic to data")
+        applyParser.add_argument("-i", "--input", dest="input", required=True, help="The input csv file")
+        applyParser.add_argument("-m", "--model", dest="model", default="preproc-model.bin", help="The preprocess model")
+        applyParser.add_argument("-o", "--output", dest="output", required=True, help="The output hdf5 file")
         # Done
         return parser.parse_args()
 
@@ -39,5 +121,9 @@ if __name__ == "__main__":
         """The main entry
         """
         args = getArguments()
+        if args.action == "train":
+            runTrain(args)
+        else:
+            runApply(args)
 
     main()
